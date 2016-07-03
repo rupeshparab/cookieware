a33c28d60d78347688838db362eca5bdb9922d96
Rupesh Parab <rupeshparab.rp@gmail.com>
2016-07-03 20:52:11 +0530

    Add user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :username, length: { in: 4..15 }
+  validates :username, length: { in: 4..15 }, uniqueness: true
   validates :password, length: { in: 6..15 }
 
 end
