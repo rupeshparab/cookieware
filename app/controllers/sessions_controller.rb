3408fc18effdf5f8ca6429d6dd467a90a797ac2f
Rupesh Parab <rupeshparab.rp@gmail.com>
2016-07-03 22:54:05 +0530

    Correct login logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
     if session.key?("user_id") && !User.find_by_id(session[:user_id]).blank?
       redirect_to '/' and return
     end
-    user = User.find_by_username(params[:username])
-    if user && user.authenticate(params[:password])
+    user = User.find_by_username(params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       redirect_to '/' and return
     else
